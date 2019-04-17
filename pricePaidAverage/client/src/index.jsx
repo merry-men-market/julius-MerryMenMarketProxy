450b0a24056f99726900e89ba8a94d2cbd1e0de3
Julius Simtim <juliussimtim@gmail.com>
2019-04-16 17:00:02 -0700

    Init

---
+++ pricePaidAverage/client/src/index.jsx
@@ -0,0 +1,11 @@
+import AveragePrice from './AveragePricePaid/averagePrice.jsx';
+import React from 'react';
+import ReactDom from 'react-dom';
+import styles from './styles.css';
+// eslint-disable-next-line no-undef
+// eslint-disable-next-line no-undef
+
+ReactDOM.render(
+ <AveragePrice/>,
+ document.getElementById('averagePrice')
+);
